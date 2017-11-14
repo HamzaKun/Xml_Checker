fdd60fda91ee757fe5daa1243fca2adaf0c41617
HamzaKun <hamzakasry@gmail.com>
2017-11-14 16:16:01 +0100

    Adding the regular expression's converter from infix to postfix form.

---
+++ src/main/java/com/kasry/core/RegExConverter.java
@@ -0,0 +1,103 @@
+package com.kasry.core;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Stack;
+
+public class RegExConverter {
+
+    /** Operators precedence map. */
+    private static final Map<Character, Integer> precedenceMap;
+    static {
+        Map<Character, Integer> map = new HashMap<Character, Integer>();
+        map.put('(', 1);
+        map.put('.', 2); // The concatenation operator
+        map.put('?', 3);
+        map.put('*', 3);
+        map.put('+', 3);
+        precedenceMap = Collections.unmodifiableMap(map);
+    };
+
+    /**
+     * Get character precedence.
+     *
+     * @param c character
+     * @return corresponding precedence
+     */
+    private static Integer getPrecedence(Character c) {
+        Integer precedence = precedenceMap.get(c);
+        return precedence == null ? 6 : precedence;
+    }
+
+    /**
+     * Transform regular expression by inserting a '.' as explicit concatenation
+     * operator.
+     */
+    private static String formatRegEx(String regex) {
+        String res = new String();
+        List<Character> allOperators = Arrays.asList('?', '+', '*');
+
+        for (int i = 0; i < regex.length(); i++) {
+            Character c1 = regex.charAt(i);
+
+            if (i + 1 < regex.length()) {
+                Character c2 = regex.charAt(i + 1);
+
+                res += c1;
+
+                if (!c1.equals('(') && !c2.equals(')') && !allOperators.contains(c2)) {
+                    res += '.';
+                }
+            }
+        }
+        res += regex.charAt(regex.length() - 1);
+
+        return res;
+    }
+
+    /**
+     * Convert regular expression from infix to postfix notation using
+     * Shunting-yard algorithm.
+     *
+     * @param regex infix notation
+     * @return postfix notation
+     */
+    public static String infixToPostfix(String regex) {
+        String postfix = new String();
+        Stack<Character> stack = new Stack<Character>();
+        String formattedRegEx = formatRegEx(regex);
+
+        for (Character c : formattedRegEx.toCharArray()) {
+            switch (c) {
+                case '(':
+                    stack.push(c);
+                    break;
+                case ')':
+                    while (!stack.peek().equals('(')) {
+                        postfix += stack.pop();
+                    }
+                    stack.pop();
+                    break;
+                default:
+                    while (stack.size() > 0) {
+                        Character peekedChar = stack.peek();
+                        Integer peekedCharPrecedence = getPrecedence(peekedChar);
+                        Integer currentCharPrecedence = getPrecedence(c);
+                        if (peekedCharPrecedence >= currentCharPrecedence) {
+                            postfix += stack.pop();
+                        } else {
+                            break;
+                        }
+                    }
+                    stack.push(c);
+                    break;
+            }
+        }
+        while (stack.size() > 0)
+            postfix += stack.pop();
+        return postfix;
+    }
+}
